b64b5ad07f7bac1ec2fe99bf46e895d2ff287f58
QATester <test@gmail.com>
2026-04-13 10:58:00 +0530

    Google test updated

--- src/test/java/com/orangehrm/tests/Google.java
+++ src/test/java/com/orangehrm/tests/Google.java
@@ -3,6 +3,8 @@ package com.orangehrm.tests;
 public class Google {
 	public static void main(String[] args) {
 		System.out.println("Launch Google webPage");
-		System.out.println("Succesfully Launched");
+		System.out.println("Succesfully Launched1");
+		System.out.println("Succesfully Launched2");
+		System.out.println("Succesfully Launched3");
 	}
 }
